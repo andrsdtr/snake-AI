34a592df79f295785a6a9450b6080d07b3f97959
Can Berkil <74599489+ihatecan@users.noreply.github.com>
2022-05-29 20:50:30 +0200

    Calculation of the fitness value

--- gen_alg.py
+++ gen_alg.py
@@ -1,6 +1,14 @@
 from random import randint, choice
 from snake_game_ai import *
 
+
+#Calculating the fitness value of the current game based on the given chromosome
 def calculate_population_fitness(pop):
     fitness = []
-  
+    for i in range(pop.shape[0]):
+        fit = play_game_with_GA(display,clock,pop[i])
+        print('Fitness value of the following chromosome' + str(i) + ': ', fitness)
+        fitness.append(fitness)
+    
+    return np.array(fitness)
+
