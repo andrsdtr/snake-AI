ba5cdd4a6d2d51b5ecf340229aa3167420e13dc3
Can Berkil <74599489+ihatecan@users.noreply.github.com>
2022-06-02 16:15:08 +0200

    transfered all functions from gen_alg.py, so the ga gets trained

--- train_ai.py
+++ train_ai.py
@@ -1,5 +1,6 @@
 from snake_game_manuel import *
 from neuronal_network import *
+from gen_alg import *
 import numpy as np
 
 #Define Initial Population for the Training
@@ -11,3 +12,25 @@ population_size = (population,amount_weights)
 
 # Create new Population based on population_size
 new_population = np.random.choice(np.arange(-1,1, step=0.01),size=population_size, replace=True)
+
+
+num_generations = 100
+
+num_parents_mating = 12
+for generation in range(num_generations):
+    print('---------------------        GENERATION ' + str(generation)+ '  ---------------------' )
+    # Measuring the fitness of each chromosome in the population.
+    fitness = calculate_population_fitness(new_population)
+    print('---------------------Fittest chromosome in the current generation ' + str(generation) +' is having the following fitness value:  ', np.max(fitness))
+    # Selecting the best parents from the current population, to use them for the next generation
+    parents = selecting_best_indiv(new_population, fitness, num_parents_mating)
+
+    # Generating the next generation with the help of crossover
+    offspring_crossover = crossover(parents, offspring_size=(population_size[0] - parents.shape[0], amount_weights))
+
+    # Offspring receives some variations with mutation
+    offspring_mutation = mutation(offspring_crossover)
+
+    # A new generation gets created based on the parents and the offspring.
+    new_population[0:parents.shape[0], :] = parents
+    new_population[parents.shape[0]:, :] = offspring_mutation
